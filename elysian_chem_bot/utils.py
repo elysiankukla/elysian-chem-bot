38b3d3d250cd6e3ae3dfa0ed3660cf829fe05cb5
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2025-02-03 23:04:45 +0800

    feat(utils): allow sanitize_message to accept iterable

--- elysian_chem_bot/utils.py
+++ elysian_chem_bot/utils.py
@@ -1,6 +1,17 @@
+from typing import Union, Iterable
+
 from elysian_chem_bot import app
 
 
-async def sanitize_message(text: str, command: str) -> str:
+async def sanitize_message(text: str, command: Union[Iterable[str], str]) -> str:
     """cleans /command or /command@username from the incoming message."""
-    return text.removeprefix(f"/{command}").removeprefix(f"@{(await app.get_me()).username}").strip()
+    username = (await app.get_me()).username
+
+    if isinstance(command, str):
+        command = [command]
+
+    for cmd in command:
+        text = text.removeprefix(f"/{cmd}")
+
+    text = text.removeprefix(f"@{username}")
+    return text.strip()
